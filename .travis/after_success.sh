59128c5d12e7af84406cfcfb2b9cac41396e4616
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2020-06-09 18:07:48 +0200

    DEBUG

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -14,19 +14,15 @@ if [[ $UPLOAD == "no-upload" ]]; then
 else
     echo "Job with Conda upload..."
 
-    if [ x$TRAVIS_BRANCH = x"master" -a x$TRAVIS_EVENT_TYPE != x"cron" -a x$TRAVIS_PULL_REQUEST == xfalse ]; then
-        $SPACER
+    $SPACER
 
-        start_section "package.upload" "${GREEN}Package uploading...${NC}"
-        if [[ -z $CUSTOM_LABEL ]]; then
-   	        anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER --label main $CONDA_OUT
-        else
-   	        anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER --label $CUSTOM_LABEL $CONDA_OUT
-        fi
-        end_section "package.upload"
+    start_section "package.upload" "${GREEN}Package uploading...${NC}"
+    if [[ -z $CUSTOM_LABEL ]]; then
+        anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER --label main $CONDA_OUT
     else
-        echo "Conditions unmet, will not upload"
+        anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER --label $CUSTOM_LABEL $CONDA_OUT
     fi
+    end_section "package.upload"
 
     $SPACER
 fi
